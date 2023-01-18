76922cd6ee9b02fb246f91aa140443b2054de2ef
QuocPham6969 <122999359+QuocPham6969@users.noreply.github.com>
2023-01-19 03:46:56 +0700

    Update config.js

--- config.js
+++ config.js
@@ -12,12 +12,12 @@ const CONFIG = {
     desc: 'Or you just wanna kill me rn 😢',
     btnYes: 'I love you a lot <33',
     btnNo: 'Lemme stab u :3',
-    question: 'There are more than 7 billion people in the world but why do you love me? :D',
+    question: 'There are more than 7 billion people in the world but why do you love me? :D (just keep typing)',
     btnReply: 'Send <3',
     reply: 'Cause you"re majestically handsome and funny too<33333333',
     mess: 'I knoww rightt🥰. You"re also very lovely too 😘😘',
     messDesc: 'I have something more to show ya',
-    btnAccept: 'Let"t go to itt <3',
+    btnAccept: 'Let"s go to itt <3',
     messLink: 'https://me-qr.com/l/asmallsurprise'
 }
 
